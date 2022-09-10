098a371a73c574c39130a24c94792fe6bbd692b9
abcoathup <andrew@coathup.net>
2022-09-10 15:11:15 +1000

    add box

--- script/QAPartyPanda.s.sol
+++ script/QAPartyPanda.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "../src/PartyPanda.sol";
 import "../src/NounsGlasses.sol";
+import "../src/Box.sol";
 import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
 contract QAPartyPandaScript is Script, ERC721Holder {
@@ -15,10 +16,15 @@ contract QAPartyPandaScript is Script, ERC721Holder {
         PartyPanda token = new PartyPanda();
         token.mint{value: token.price()}();
 
+        Box background = new Box();
+        background.mint{value: background.price()}();
+
         NounsGlasses accessory = new NounsGlasses();
         accessory.mint{value: accessory.price()}();
 
-        console.log(accessory.tokenURI(1));
+        background.approve(address(token), 1);
+        token.addBackground(1, address(background), 1);
+
 
         accessory.approve(address(token), 1);
         token.addAccessory(1, address(accessory), 1);
